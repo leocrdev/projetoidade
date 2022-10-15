c113e552d9cccfcc1d10c3aad733e6f52fe652d8
LEOCRDEV leocrdev@hotmail.com <LEOCRDEV leocrdev@hotmail.com>
2022-10-14 21:41:11 -0300

    projeto Idade

---
+++ projetoridade/src/projetoridade/projectidade.java
@@ -0,0 +1,25 @@
+package projetoridade;
+
+public class projectidade {
+
+	/*Main é um método auto executável em Java*/
+	public static void main(String[] args) {
+		
+	int maiorIdade = 18;
+	
+	int idosoIdade = 60;
+	
+	int jovemIdade = 16;
+	
+	int criancaIdade = 7;
+	
+	String  cpf = "145.785.789-54";
+	
+	System.out.println("Maior idade é = " + maiorIdade);
+	System.out.println("Idade idoso é = " + idosoIdade);
+	System.out.println("Idade jovem é = " + jovemIdade);
+	System.out.println("Idade criança é " + criancaIdade);
+	
+	}
+	
+}
